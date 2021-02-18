8845e1cf9cddfa333e497fb18d843a30f9916b4d
vShlapachenko <valentyn.shlapachenko@gmail.com>
2021-02-17 18:08:27 -0800

    add-multi-modular-structure
    Changed path. Needs default folder renaming

--- lazy-tester/src/main/java/com/company/Main.java
+++ lazy-tester/src/main/java/com/company/Main.java
@@ -28,7 +28,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         var classPath = CodeGenerationUtils
                 .mavenModuleRoot(BestEverService.class)
-                .resolve("lazy-tester/src/main/java/com/company/demo");
+                .resolve("src/main/java/com/company/demo");
         var compilationUnit = new SourceRoot(classPath).parse("", "BestEverService.java");
         compilationUnit.addImport(BlockComment.class);
         compilationUnit.accept(new VoidVisitorAdapter<>() {
